5fa645054a35fe60bc69ad39b7859bf260d0812d
MicahZhang <m_zhang@outlook.com>
2017-05-05 23:38:11 +0000

    simplified camera code

--- CameraFirstTry.py
+++ CameraFirstTry.py
@@ -1,23 +1,49 @@
 from picamera import PiCamera
 from time import sleep
+from PIL import Image
 
+#INITIALIZE CAMERA
 camera = PiCamera()
 
-#Video 10 sec
-camera.start_preview()
-sleep(60)
+#RESIZE PREVIEW WINDOW
+x = 0 #horizontal distance of preview window from upper left corner
+y = 530 #vertial ''
+width = 620 #width of preview window
+height = 480 #height ''
+
+#DISPLAY CAMERA PREVIEW IN LOWER-LEFT CORNER WINDOW
+filename = '/home/pi/Desktop/image.jpg' #specify 
+camera.start_preview(fullscreen=False,window = (x,y,width,height))
+while(1):
+    ans = raw_input("Enter 'quit' to end preview. Enter 'pic' to take an image. Enter 'invert' to invert the preview: ")
+    if ans == "quit":
+        print("\nquitting \n")
+        break
+    elif ans == "pic":
+        print("\ntaking picture \n")
+        camera.capture(filename)
+        im = Image.open(filename)
+        im.show(im)
+    elif ans == "invert":
+        print("\ninverting\n")
+        camera.stop_preview() 
+        camera.vlip = False
+        camera.start_preview(fullscreen=False,window = (x,y,width,height))
+
+#END PREVIEW    
 camera.stop_preview() 
+
 
 #One image
 #camera.start_preview()
 #sleep(3)
 #camera.capture('/home/pi/Desktop/image.jpg')
-#camera.stop_preview()
+#camera.stop_peview()
 
 #Resolution
 #camera.resolution = (2593, 1944)
 #camera.framerate = 15
-#camera.start_preview()
+#camera.start_prevew()
 
 #sleep(5)
 #camera.capture('/home/pi/Desktop/text.jpg')
