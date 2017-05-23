3a749a336173e46486d8f23f12add58719dced56
MicahZhang <m_zhang@outlook.com>
2017-05-23 17:17:45 +0000

    new accelerometer code and revised humidifier code

--- accel.py
+++ accel.py
@@ -2,20 +2,28 @@ import smbus
 import time
 
 bus = smbus.SMBus(1) #initialization 
-address = 0x40 #sensor specific
+address = 0x53 #sensor specific
 
 ## List of commands:
-# 0xE5: Measure RH, Hold Master Mode
-# 0xF5 Measure RH, No Hold Master Mode (DEFAULT)
-# 0xE3: Measure Temp, Hold Master Mode
-# 0xF3: Measure Temp, No Hold Master Mode (DEFAULT)
-# 0xE0: Read Temperature Value from Previous RH Measurement
-# 0xFE: Reset  
+# 0x32: X-Axis LSB (Least Significant Bit)
+# 0x33  X-Axis MSB (Most Significant Bit) 
+# 0x34: Y-Axis LSB
+# 0x35: Y-Axis MSB
+# 0x36: Z-Axis LSB
+# 0x37: Z-Axis MSB  
+
+def xaxis():
+	LSB  = bus.read_byte_data(address,0x32)
+	MSB = bus.read_byte_data(address,0x33)
+	
+
+
+
 
 #reads raw data
 def raw():
-	raw = bus.read_word_data(address,command)
-	return raw
+	raw = bus.read_byte_data(address,command)
+	return raw 
 
 #convert raw to relative humidity
 def rh(raw):
@@ -36,13 +44,13 @@ while True:
 		if master == 'HM':
 			command = 0xE5
 			while True:
-				time.sleep(0.5) 
+				time.sleep(1) 
 				a = raw()
 				b = rh(a)
 				print(a,hex(int(a)),b,hex(int(b)))
 		else:
 			while True:
-				time.sleep(0.5)
+				time.sleep(1)
 				a = raw()
 				b = rh(a) 
 				print(a,hex(int(a)),b,hex(int(b)))
