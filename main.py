8733e7014c0470dc87c34a57c58182933cbfdc1e
Micah Zhang <micah.zhang@colorado.edu>
2017-06-05 14:54:40 +0000

    changed imports

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import threading
 import time 
 import queue
-from camera import TESTcam
+from camera import camera
 from downlink import TESTdwlk
-from motor import TESTmotor
-from sensors import TESTsensor
+from motor import MImotor
+from sensors import SENSORINTbby
 
 q = queue.Queue()
 
-t1  = threading.Thread(name='came', target=TESTcam.main)
+t1  = threading.Thread(name='came', target=camera)
 t2 =  threading.Thread(name='dwlk', target=TESTdwlk.main) 
-t3  = threading.Thread(name='moto', target=TESTmotor.main)
-t4 = threading.Thread(name='sens', target=TESTsensor.main)  
+t3  = threading.Thread(name='moto', target=MImotor)
+t4 = threading.Thread(name='sens', target=SENSORINTbby)  
 
 t1.start()
 t2.start()
 t3.start()
 t4.start()
-
-#print(TESTcam.q.get())
-#print(TESTdwlk.q.get())
 
 print("Main Completed")
