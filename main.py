b41fc16c7d0b87b860d1a3adc4a6708a0df2780b
Clk415723! <coke6683@colorado.edu>
2017-06-09 15:24:09 +0000

    motor and camera code for systems

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import threading
 import time 
 import queue
 from cama import cama
-from moto import moto 
+from moto import OUTmoto 
 from sens import sens
 from dwlk import dwlk
  
 tsens = threading.Thread(name='sens', target=sens.main)  
 tdwlk = threading.Thread(name='dwlk', target=dwlk.main)
-tmoto = threading.Thread(name='moto', target=moto.main)
+tmoto = threading.Thread(name='moto', target=OUTmoto.main)
 tcama = threading.Thread(name='cama', target=cama.main)
 
 tsens.start()
