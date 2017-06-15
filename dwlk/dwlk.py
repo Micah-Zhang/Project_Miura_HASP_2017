2dbc815a864ff8df4338d730c2d23542fb492c99
MicahZhang <m_zhang@outlook.com>
2017-06-15 16:07:25 -0600

    DWLK

--- dwlk/dwlk.py
+++ dwlk/dwlk.py
@@ -2,11 +2,15 @@ import threading
 import serial
 import time
 import queue
+import sys
+sys.path.append('/home/pi/miura')
+import func
+from func import *
 
 q = queue.Queue()
 
 ser = serial.Serial(
-	port = '/dev/ttyserial0',
+	port = '/dev/serial0',
 	baudrate = 4800,
 	parity = serial.PARITY_NONE,
 	stopbits = serial.STOPBITS_ONE,
@@ -16,9 +20,14 @@ ser = serial.Serial(
 
 ser.close()
 ser.open()
-
 def main():
+	counter = 0
 	while True:
-		a = q.get()		
-		b = ser.write(bytes(a, encoding="UTF-8"))
-		print("packet sent")
+#		a = q.get()		
+		a = "hi micah\n"
+		#b = ser.write(bytes(a,'utf-8'))
+		ser.write(a.encode('utf-8'))
+		print("packet sent",counter)
+		time.sleep(1)
+		counter+=1
+main()
