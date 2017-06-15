a409fcc00526eba31a864065c1a12e08d996d01f
MicahZhang <m_zhang@outlook.com>
2017-06-15 10:26:14 -0600

    All sensor code working

--- sens/sens.py
+++ sens/sens.py
@@ -12,16 +12,21 @@ def main():
 	while True:
 		label = 'CU ' + 'MI ' + 'SE '
 		timestamp = "{0:.2f}".format(time.time())
-		rangefinder, ammeter = func.read_adc()
+		rangefinder,ammeter = func.read_adc()
 		rangefinder = ' ' + "{0:.2f}".format(rangefinder)
 		ammeter = ' ' + "{0:.2f}".format(ammeter)
 		humidity = ' ' + "{0:.2f}".format(func.read_humi())
 		pressure = ' ' + "{0:.2f}".format(func.read_pres())
-		temp1 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor1))
-		temp2 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor2))
-		temp3 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor3))
-		#data = rangefinder + ammeter + humidity + temp1 + temp2 + temp3
-		data = rangefinder + humidity + pressure + temp1
+		#temp1 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor1))
+		temp = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor))
+		#temp3 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor3))
+#		temp4 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor4))
+		accel = ' ' + "{0:.2f}".format(func.read_acc())
+		
+		data = rangefinder + ammeter + humidity + pressure + temp + accel 
+		#data = rangefinder + ammeter + humidity + temp2 + temp4
+		#data = humidity + pressure + temp
+		#data = temp2
 		checksum = ' ' + str(adler32(data.encode('utf-8')) & 0xffffffff)
 		packet = label + timestamp + checksum + data
 		print(packet)
@@ -29,3 +34,5 @@ def main():
 		dwlk.q.put(packet)
 		#name = 'test{:d}.log'.format(counter)
 		#func.save_file(name,packet)
+
+#main()
