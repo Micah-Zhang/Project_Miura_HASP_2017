88cce54279214c76222190950e9554aa1becdd91
MicahZhang <m_zhang@outlook.com>
2017-06-15 16:08:00 -0600

    MAIN

--- main.py
+++ main.py
@@ -2,24 +2,19 @@ import threading
 import time
 import queue
 from cama import cama
-<<<<<<< HEAD
-from moto import OUTmoto 
-=======
-from moto import moto
->>>>>>> 7f3171bee71449182dedb0b8882c53070e03de7b
+#from moto import OUTmoto 
+#from moto import moto
 from sens import sens
 from dwlk import dwlk
-import sys
-sys.path.append('/home/pi/miura/uplk')
-import uplk
+from uplk import uplk
 
 q = queue.Queue()
 
 tuplk = threading.Thread(name='uplk', target=uplk.main)
 tsens = threading.Thread(name='sens', target=sens.main)  
 tdwlk = threading.Thread(name='dwlk', target=dwlk.main)
-tmoto = threading.Thread(name='moto', target=OUTmoto.main)
-tcama = threading.Thread(name='cama', target=cama.main)
+#tmoto = threading.Thread(name='moto', target=OUTmoto.main)
+#tcama = threading.Thread(name='cama', target=cama.main)
 
 tuplk.start()
 print("uplk started")
@@ -27,10 +22,7 @@ tdwlk.start()
 print("dwlk started")
 tsens.start()
 print("sens started")
-while True:
-	if q.get() == "start\n":
-		break
-tmoto.start()
-print("motor started")
-tcama.start()
-print("camera started")
+#tmoto.start()
+#print("motor started")
+#tcama.start()
+#print("camera started")
