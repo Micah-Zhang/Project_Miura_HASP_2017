e302f763848864e5dd4486586325482df6beae67
Micah Zhang <micah.zhang@colorado.edu>
2017-06-15 21:04:31 -0600

    changed

--- sens/sens.py
+++ sens/sens.py
@@ -1,35 +1,33 @@
-import datetime
+import time
 from zlib import adler32
 import sens.sensors as sensors
 
-'''
 if __name__ == '__main__':
 	import sensors
 	main()
 else:
 	import sens.sensors as sensors
-'''
 
 def main(downlink):
 	counter = 1
 	while True:
 		label = 'CU ' + 'MI ' + 'SE '
 		timestamp = "{0:.2f}".format(time.time())
-		#rangefinder,ammeter = func.read_adc()
+		#rangefinder,ammeter = sensors.read_adc()
 		#rangefinder = ' ' + "{0:.2f}".format(rangefinder)
 		#ammeter = ' ' + "{0:.2f}".format(ammeter)
-		#humidity = ' ' + "{0:.2f}".format(func.read_humi())
-		#pressure = ' ' + "{0:.2f}".format(func.read_pres())
-		#temp1 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor1))
-		#temp = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor))
-		#temp3 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor3))
-		#temp4 = ' ' + "{0:.2f}".format(func.read_temp(temp_sensor4))
-		#accel = ' ' + "{0:.2f}".format(func.read_acc())
+		#humidity = ' ' + "{0:.2f}".format(sensors.read_humi())
+		#pressure = ' ' + "{0:.2f}".format(sensors.read_pres())
+		#temp1 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor1))
+		#temp = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor))
+		#temp3 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor3))
+		#temp4 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor4))
+		#accel = ' ' + "{0:.2f}".format(sensors.read_acc())
 		#data = humidity + temp + rangefinder + ammeter
 		#data = rangefinder + humidity + pressure + temp + accel
 		#data = humidity + pressure + temp
 		#data = temp2
-		#data = "junk"
+		data = "junk"
 		checksum = ' ' + str(adler32(data.encode('utf-8')) & 0xffffffff)
 		packet = label + timestamp + checksum + data
 		print(packet)
