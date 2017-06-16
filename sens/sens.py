6a80ac13356758c3d18c2345eed1281fbb557ec3
MicahZhang <m_zhang@outlook.com>
2017-06-15 21:34:22 -0600

    IT STILL WORKS

--- sens/sens.py
+++ sens/sens.py
@@ -7,30 +7,31 @@ if __name__ == '__main__':
 	main()
 else:
 	import sens.sensors as sensors
+	from sens.sensors import *
 
 def main(downlink):
 	counter = 1
 	while True:
 		label = 'CU ' + 'MI ' + 'SE '
 		timestamp = "{0:.2f}".format(time.time())
-		#rangefinder,ammeter = sensors.read_adc()
-		#rangefinder = ' ' + "{0:.2f}".format(rangefinder)
-		#ammeter = ' ' + "{0:.2f}".format(ammeter)
-		#humidity = ' ' + "{0:.2f}".format(sensors.read_humi())
+		rangefinder,ammeter = sensors.read_adc()
+		rangefinder = ' ' + "{0:.2f}".format(rangefinder)
+		ammeter = ' ' + "{0:.2f}".format(ammeter)
+		humidity = ' ' + "{0:.2f}".format(sensors.read_humi())
 		#pressure = ' ' + "{0:.2f}".format(sensors.read_pres())
 		#temp1 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor1))
-		#temp = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor))
+		temp = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor))
 		#temp3 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor3))
-		#temp4 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor4))
+		temp4 = ' ' + "{0:.2f}".format(sensors.read_temp(temp_sensor4))
 		#accel = ' ' + "{0:.2f}".format(sensors.read_acc())
 		#data = humidity + temp + rangefinder + ammeter
-		#data = rangefinder + humidity + pressure + temp + accel
+		data = rangefinder + ammeter +  humidity +  temp + temp4
 		#data = humidity + pressure + temp
 		#data = temp2
-		data = "junk"
+		#data = "junk"
 		checksum = ' ' + str(adler32(data.encode('utf-8')) & 0xffffffff)
 		packet = label + timestamp + checksum + data
-		print(packet)
+#		print(packet)
 		packet = packet + '\n'
 		downlink.put(packet)
 		name = 'test{:d}.log'.format(counter)
