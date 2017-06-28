b922e192b1b6ce08eda1ce5ae7a4f2f77e80e8ee
MicahZhang <m_zhang@outlook.com>
2017-06-28 15:51:04 -0600

    webcam stufff

--- webcam/webcamTEST.py
+++ webcam/webcamTEST.py
@@ -18,15 +18,14 @@ import time
 pic = 0
 
 while pic<6:
-#    time.sleep(5)
-#    os.system('fswebcam -d /dev/video0 --save blackimage{}.jpg'.format(pic))
-#    pic += 1
-#    print("Image taken")
     time.sleep(5)
-    os.system('fswebcam -d /dev/video0 --save redimage{}.jpg'.format(pic))
+    os.system('fswebcam -d /dev/video0 --save blackimage{}.jpg'.format(pic))
     pic += 1
-    print("Second Image taken")
-#    time.sleep(5)
+    '''
+    time.sleep(5)
+    os.system('fswebcam -d /dev/video1 --save redimage{}.jpg'.format(pic))
+    pic += 1
+    '''
 
 
 
