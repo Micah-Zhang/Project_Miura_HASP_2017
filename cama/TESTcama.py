87859ac5b9d2692f1d51401b436ed0c8afa1a2f4
Micah Zhang <micah.zhang@colorado.edu>
2017-06-30 13:35:14 -0600

    cleaning up

--- cama/TESTcama.py
+++
@@ -1,6 +0,0 @@
-import queue
-q = queue.Queue()
-
-def main():
-	print("camera thread verified")
-	q.put("camera queued")
--- test/thread.py
+++
@@ -1,5 +0,0 @@
-from threading import Thread
-import time
-
-def timer(name, delay, repeat):
-	print("Timer
