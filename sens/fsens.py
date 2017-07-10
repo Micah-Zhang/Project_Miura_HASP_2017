b2bbd249ee8237824a2abffe1709f761968cdc83
Micah Zhang <micah.zhang@colorado.edu>
2017-07-10 15:19:08 -0600

    updated sensor function code to work with new downlink format

--- sens/fsens.py
+++ sens/fsens.py
@@ -2,11 +2,13 @@ import os
 import time
 import smbus
 from smbus import SMBus
-#import Adafruit_ADXL345
 import RPi.GPIO as GPIO
-#import Adafruit_ADS1x15
 import math
+#import Adafruit_ADXL345
+#import Adafruit_ADS1x15
 
+
+# Setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 os.system('modprobe w1-gpio')
@@ -18,97 +20,116 @@ temp_sensor4 = '//sys/bus/w1/devices/28-000007a8acab/w1_slave'
 temp_sensor5 = '//sys/bus/w1/devices/28-000007a8af78/w1_slave'
 temp_sensor6 = '//sys/bus/w1/devices/28-000007a8b7a1/w1_slave'
 temp_sensor7 = '//sys/bus/w1/devices/28-000007a8c380/w1_slave'
-#accel = Adafruit_ADXL345.ADXL345()
-#adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
 bus = smbus.SMBus(1)
+
+# Extra Setup
+#accel = Adafruit_ADXL345.ADXL345()
+#adc = Adafruit_ADS1x15.ADS1115()
 #bus.write_byte_data(0x60, 0x26, 0x39) #pres
 
+
+# Handles sensor reading schedule
+class PeriodicScheduler:
+	def __init__(self):
+		self.scheduler = sched.scheduler(time.time, time.sleep)
+
+	def setup(self, interval, action, actionargs=()):
+		self.scheduler.enter(interval, 1, self.setup, (interval, action, actionargs))
+		action(*actionargs)
+
+	def run(self):
+		self.scheduler.run()
+
+
+# Converts data array into string
+def cs_str(data):
+	out = "" # Initialize variable with empty string
+	for i in range(len(data)): # Iterate through each "word" in data
+		out += "%f " % (data[i]) # Add each word to the string "out"
+	# out += "%f" % (data[-1])
+	return out # Return newly formed string
+
+
+# HELIOSV's temp sensor read function. I strongly prefer this over ours. 
+'''
+def temp(downlink, tempLED):
+	try:
+		data_raw = []
+		data = []
+		for sensor in W1ThermSensor.get_available_sensors(): # Grab temp values from all available sensors in a round robin fashion
+			data_raw.append(sensor.get_temperature())
+		for i in range(len(data_raw)): # Copy raw temp values into file meant for converted values. Was not fully implemented.
+			data.append(data_raw[i])
+			#data.append(temp_cal[i] + data_raw[i])
+		if (not tempLED.is_set()) and tempCheck(data): # If any temp sensors are overheating and the temp LED is not on, turn it on.
+			# If the flag isn't set, and things are on fire.
+			tempLED.set()
+		downlink.put(["SE", "T%i" % (len(data)), cs_str(data)]) # Send the packaged data packet to the downlink thread.
+	except:
+		print("Temperature reading failed")
+'''
+
+
+# Grab raw temp data
 def temp_raw(temp_sensor):
 	f = open(temp_sensor, 'r')
 	lines = f.readlines()
 	f.close()
 	return lines
 
-def read_temp(temp_sensor): #read temperature
-	lines = temp_raw(temp_sensor)
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = temp_raw(temp_sensor)
-	temp_output = lines[1].find('t=')
-	if temp_output != -1:
-		temp_string = lines[1].strip()[temp_output+2:]
-		temp_c = float(temp_string) /1000.0
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
-		return temp_f	
+# Process raw data and return nice data for ONE temp sensor. Will replace with HELIOSV code once verified.
+def read_temp(downlink):
+	try:
+		lines = temp_raw(temp_sensor1)
+		while lines[0].strip()[-3:] != 'YES':
+			time.sleep(0.2)
+			lines = temp_raw(temp_sensor1)
+		temp_output = lines[1].find('t=')
+		if temp_output != -1:
+			temp_string = lines[1].strip()[temp_output+2:]
+			temp_c = float(temp_string) /1000.0
+			temp_f = temp_c * 9.0 / 5.0 + 32.0
+			downlink.put(["SE", "T1", "{0:.2f}".format(temp_f)])
+	except:
+		print("Temperature reading failed")
 
-def read_humi(): #read humidity
-	bus.write_byte(0x40, 0xF5)
-	time.sleep(0.3)
-	data0 = bus.read_byte(0x40)
-	data1 = bus.read_byte(0x40)
-	humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
-	time.sleep(0.3)
-	return humidity
-'''
-def read_pres():
-        deg = u'\N{DEGREE SIGN}'
-        ADDR = 0x60
-        Ctrl_REG1 = 0x26
-        PT_DATA_CFG = 0x13
-        bus = SMBus(1)
 
-        whoAmI = bus.read_byte_data(ADDR, 0x0C)
-        print( hex(whoAmI))
-        if whoAmI != 0xc4:
-                print( "Device Not Active")
-                exit(1)
-        setting = bus.read_byte_data(ADDR, CTRL_REG1)
-        newSetting = setting | 0x38
-        bus.write_byte_data(ADDR, CTRL_REG1, newSetting)
+# Grab raw data from bus. Convert raw data to nice data.
+def read_humi(downlink):
+	try:
+		bus.write_byte(0x40, 0xF5)
+		time.sleep(0.3)
+		data0 = bus.read_byte(0x40)
+		data1 = bus.read_byte(0x40)
+		humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+		time.sleep(0.3)
+		downlink.put(["SE", "HU", "{0:.2f}".format(humidity)])
+	except:
+		pass
 
-        bus.write_byte_data(ADDR, PT_DATA_CFG, 0x07)
 
-        setting = bus.read_byte_data(ADDR, CTRL_REG1)
-        if (setting & 0x02) == 0:
-                bus.write_byte_data(ADDR, CTRL_REG1, (setting | 0x02))
+# Grab raw data from bus. Convert raw data to nice data.
+def read_pres(downlink):
+	try:
+		data = bus.read_i2c_block_data(0x60, 0x00, 4)
+		pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+		pressure = (pres / 4.0) / 1000.0
+		downlink.put(["SE", "PR", "{0:.2f}".format(humidity)])
+	except:
+		pass
 
-        print( "Waiting for data")
-        status = bus.read_bytes_data(ADDR,0x00)
-        time.sleep(0.5)
+# Grab raw data from bus. Use Adafruit library to convert to nice data.
+def read_acce(downlink):
+	try:
+		x, y, z = accel.read()
+		downlink.put(["SE", "AC", "{0:.2f}".format(x), "{0:.2f}".format(y), "{0.2f}".format(z)])
+	except:
+		pass
 
-        print( "Reading data")
-        p_data = bus.read_i2c_block_data(ADDR,0x01,3)
-        t_data = bus.read_i2c_block_data(ADDR,0x04,2)
-        status = bus.read_byte_data(ADDR,0x00)
-        print("status:" +bin(status))
 
-        p_msb = p_data[0]
-        p_csb = p_data[1]
-	p_lsb = p_data[2]
-        t_msb = t_data[0]
-        t_lsb = t_data[1]
-
-        pressure = (p_msb << 10) | (p_csb << 2) | (p_lsb >> 6)
-        p_decimal = ((p_lsb & 0x30) >>4) / 4.0
-
-        print("Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z'))
-        print( str(pressure + p_decimal))
-
-	
-
-'''
-def read_pres(): #read pressure
-	data = bus.read_i2c_block_data(0x60, 0x00, 4)
-	pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-	pressure = (pres / 4.0) / 1000.0
-	return pressure
-
-def read_acc(): #read accelerometer
-	x, y, z = accel.read()
-	return x, y, z
-
-def blink(pin): #blink LEDs
+# Blink LEDs with 0.5 second spacing.
+def blink(pin):
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin,GPIO.HIGH)
 	time.sleep(0.5)
@@ -116,25 +137,16 @@ def blink(pin): #blink LEDs
 	time.sleep(0.5)
 	return
 
-def led_on(pin): #turn on LEDs	
+# Turn LEDs on
+def led_on(pin):
 	GPIO.output(pin,GPIO.HIGH)
 
-def led_off(pin): #turn off LEDs
+# Turn off LEDs
+def led_off(pin):
 	GPIO.output(pin,GPIO.LOW)
 
-def read_adc(): #read ADC
-	values = [0]*4
-	for i in range(4):
-		values[i] = adc.read_adc(i, gain=GAIN)
-	#Rangefinder conversion factor
-	values[0] = 130*math.pow(values[0]/1000,-1.1)
-	#Ammeter Conversion factor
-	Vref = 20000
-	sensitivity = 500/5000
-	voltage = 4.88*(values[1])
-	values[1] = (voltage - Vref) * sensitivity / 10
-	return values[0], values[1]
 
+# Save file
 def save_file(filename, data):
 	with open(filename, "a+") as f:
 		f.write(data)
