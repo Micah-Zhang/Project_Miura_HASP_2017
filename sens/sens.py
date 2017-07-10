9ac77c58958b8402c753db9a21e3484385632689
Micah Zhang <micah.zhang@colorado.edu>
2017-07-10 15:39:46 -0600

    converted sens.py to work with new downlink format

--- sens/sens.py
+++ sens/sens.py
@@ -1,38 +1,19 @@
-import time
-from zlib import adler32
+import sens.fsens as fsens
+
+# Sets sampling frequency for all sensors (in seconds)
+TEMP_INTERVAL = 10
+PRES_INTERVAL = 10
+HUMI_INTERVAL = 10
+ACCE_INTERVAL = 10
+
 
 def main(downlink):
-	counter = 1
-	while True:
-		label = 'CU ' + 'MI ' + 'SE '
-		timestamp = "{0:.2f}".format(time.time())
-		#rangefinder,ammeter = fsens.read_adc()
-		#rangefinder = ' ' + "{0:.2f}".format(rangefinder)
-		#ammeter = ' ' + "{0:.2f}".format(ammeter)
-		#humidity = ' ' + "{0:.2f}".format(fsens.read_humi())
-		#pressure = ' ' + "{0:.2f}".format(fsens.read_pres())
-		#temp1 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor1))
-		#accel = ' ' + "{0:.2f}".format(fsens.read_acc())
-		#data = rangefinder + ammeter +  humidity + pressure + temp1 + accel
-		temp1 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor1))
-		temp2 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor2))
-		temp3 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor3))
-		temp4 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor4))
-		temp5 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor5))
-		temp6 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor6))
-		temp7 = ' ' + "{0:.2f}".format(fsens.read_temp(temp_sensor7))
-		data = temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7
-		checksum = ' ' + str(adler32(data.encode('utf-8')) & 0xffffffff)
-		packet = label + timestamp + checksum + data
-		packet = packet + '\n'
-		downlink.put(packet)
-		name = 'test{:d}.log'.format(counter)
-		fsens.save_file(name,packet)
 
-if __name__ == '__main__':
-	import fsens
-	from fsens import *
-	main()
-else:
-	import sens.fsens as fsens
-	from sens.fsens import *
+	scheduler = fsens.PeriodicScheduler() # Create a scheduler object that accesses the sensors file
+	scheduler.setup(TEMP_INTERVAL, fsens.read_temp, downlink) # Set up temp sensors for scheduling, passing necessary argument 
+	scheduler.setup(PRES_INTERVAL, fsens.read_pres, downlink) # Set up pressure sensors for scheduling, passing necessary arguments
+	scheduler.setup(HUMI_INTERVAL, fsens.read_humi, downlink) # Set up humidity sensor for scheduling, passing necessary arguments
+	scheduler.setup(ACCE_INTERVAL, fsens.read_acce, downlink) # Set up accelerometer for scheduling, passing necessary arguments
+
+	scheduler.run() # Begin scheduling all sensors
+	downlink.put(["SE", "BU", "SENS"]) # Verify that sensor thread started properly
