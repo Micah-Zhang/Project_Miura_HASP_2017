6dacce9b682973f52f14750a969233efa4f0a461
Micah Zhang <micah.zhang@colorado.edu>
2017-07-17 10:28:02 -0600

    added print statements for troubleshooting

--- moto/fmoto.py
+++ moto/fmoto.py
@@ -18,8 +18,10 @@ def move(steps):
 		#stop moving if either button depressed
 		if GPIO.input(cmoto.Upper_Button) or GPIO.input(cmoto.Lower_Button):
 			if GPIO.input(cmoto.Upper_Button): #figure out which button was pressed and then reset stepcount accordingly
+				print("Upper button pressed")
 				cmoto.max_step = cmoto.step_count
 			else:
+				print("Lower button pressed")
 				cmoto.step_count = 0
 			return
 		#otherwise, move motor and increase step count
@@ -42,19 +44,24 @@ def take_4_images():
 def checkUplink(moto_cmd, downlink):
 	while not moto_cmd.empty(): #grab commands until queue empty
 		cmd = moto_cmd.get()
+		print("command received")
 		if type(cmd) is bytes:
 			packet = hex(int.from_bytes(cmd, byteorder='big'))
 			if cmd == b"\x01":
+				print("setting bot_calib flag as TRUE")
 				cmoto.bot_calib = True
 				downlink.put(["MO","AK",packet])
 			elif cmd == b"\x02":
+				print("setting top_calib flag as TRUE")
 				cmoto.top_calib = True
 				downlink.put(["MO","AK",packet])
 			elif cmd == b"\x03":
+				print("setting minimum_success flag as TRUE")
 				cmoto.minimum_success = True
 				cmoto.cycle_start_time = time.time()
 				downlink.put(["MO","AK",packet])
 			elif cmd == b"\x04":
+				print("setting full_extension flag as TRUE")
 				cmoto.full_extension = True
 				cmoto.cycle_start_time = time.time()
 				downlink.put(["MO","AK",packet])
@@ -66,12 +73,16 @@ def checkUplink(moto_cmd, downlink):
 			if abs(cmd) <= 100:
 				#cmoto.nudge_step = ((cmd/100)-(cmoto.step_count/cmoto.max_step))*cmoto.max_step)
 				cmoto.nudge_step = (cmd*(cmoto.max_step/100)) - cmoto.step_count
+				print("nudge_step defined")
 				cmoto.nudge_state = True #signal ready for nudging
+				print("setting nudge_state flag a TRUE")
 				downlink.put(["MO","AK",str(cmd)])
 			elif abs(cmd) > 100:
 				cmd = cmd - 101
 				cmoto.nudge_step = cmd*(cmoto.max_step/100)
+				print("nudge_step defined")
 				cmoto.nudge_state = True #signal ready for nudging
+				print("setting nudge_state flag as TRUE")
 				downlink.put(["MO","AK",str(cmd)])
 			else:
 				downlink.put(["MO", "ER",str(cmd)])
