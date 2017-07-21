bba9cff77364fbb0470c021cc919988bb5f79aac
MicahZhang <m_zhang@outlook.com>
2017-07-21 16:15:36 -0600

    deleted extra logs

--- dwlk/dwlk.py
+++ dwlk/dwlk.py
@@ -4,22 +4,22 @@ from zlib import adler32
 # Stores data from each thread in separate logs
 def logdata(packet, sender):
 	if sender == "MO": # Keeps track of all packets sent by the motor thread
-		with open("/home/pi/miura/moto.log", 'a') as log:
+		with open("/home/pi/moto.log", 'a') as log:
 			log.write(packet)
 		print(packet, end="")
 
 	if sender == "UP": # Keeps track of all packets sent by the uplink thread
-		with open("/home/pi/miura/uplk.log", 'a') as log:
+		with open("/home/pi/uplk.log", 'a') as log:
 			log.write(packet)
 		print(packet, end="")
 
 	if sender == "SE": # Keeps track of all packets sent by the sensor thread
-		with open("/home/pi/miura/sens.log", 'a') as log:
+		with open("/home/pi/sens.log", 'a') as log:
 			log.write(packet)
 		print(packet, end="")
 
 	if sender == "DW": # Keeps track of all packets sent by the downlink thread
-		with open("/home/pi/miura/dwlk.log", 'a') as log:
+		with open("/home/pi/dwlk.log", 'a') as log:
 			log.write(packet)
 		print(packet, end="")
 
@@ -40,7 +40,7 @@ def main(downlink, gnd):
 		ck = adler32(a_data) & 0xffffffff # Use data to calculate checksum
 		t = time.time()  # Unix time. Seconds since epoch.
 		packet = "\x01CU MI %s %s %.2f %i %i\x02" % (sender, record, t, l, ck) + " " + data + "\x03\n" # Follows HASP guidelines
-		with open("/home/pi/miura/downlink.log", 'a') as log: # Keeps a central record of everything that was downlinked
+		with open("/home/pi/downlink.log", 'a') as log: # Keeps a central record of everything that was downlinked
 			log.write(packet)
 		gnd.write(packet) # Downlink packet through serial
 #		print("post-packet: ", packet)	
