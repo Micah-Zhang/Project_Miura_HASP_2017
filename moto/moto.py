c0191e2761cf44f9df94a9b724f7a2dd105a9721
Micah Zhang <micah.zhang@colorado.edu>
2017-07-31 10:06:40 -0600

    full extension now goes to 1.2%. full_extension flag resets

--- moto/moto.py
+++ moto/moto.py
@@ -70,7 +70,7 @@ def main(downlink, moto_cmd, safe_mode, cam_is_moving, cam_is_open, cam_reset):
 				if cmoto.minimum_success:
 					fmoto.move(int(73*(cmoto.max_step/100) - cmoto.step_count), downlink, safe_mode, encoder)
 				else:
-					fmoto.move(cmoto.max_step - cmoto.step_count, downlink, safe_mode, encoder)
+					fmoto.move(cmoto.max_step*1.2 - cmoto.step_count, downlink, safe_mode, encoder)
 				cmoto.motor_start_time = time.time()
 				cam_is_moving.clear()
 				cam_reset.set()
@@ -89,6 +89,7 @@ def main(downlink, moto_cmd, safe_mode, cam_is_moving, cam_is_open, cam_reset):
 				cmoto.cycle_extended = False
 				cmoto.cycle_contracted = False
 				cmoto.minimum_success = False
+				cmoto.full_extension = False
 				cmoto.cycle_end_time = time.time()
 		if not cmoto.auto_set and (time.time() > cmoto.mission_start_time + cmoto.auto_wait): #begin automation after set amount of time
 			cmoto.auto_set = True
