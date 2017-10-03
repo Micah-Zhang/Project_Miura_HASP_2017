10aa82b7912accf102ba17a593b07b55c7df5950
Micah Zhang <micah.zhang@colorado.edu>
2017-10-02 18:58:51 -0600

    added data files

--- analysis/mat_a.m
+++ analysis/mat_a.m
@@ -3,5 +3,4 @@ close all;
 clc;
 
 fileName = '/home/micah/miura/analysis/data1.raw';
-data = textread(fileName,'%s','delimiter','\n');
-ftell(data)
+data = textread(fileName,'%c','delimiter','\n');
--- analysis/py_a.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('data1.raw') as file:
-	data = file.read()
-print(re.escape(data))
-
-
-#print(re.search(data,'BU'))
