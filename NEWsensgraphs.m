ce7742c6f29599912b7afc65d56ba01a1248f044
coke6683 <coke6683@colorado.edu>
2017-11-14 23:06:53 -0700

    Add files via upload

--- NEWsensgraphs.m
+++ NEWsensgraphs.m
@@ -1,14 +1,75 @@
-%clear all;
-%close all;
-%clc;
+%New sensor temperature graphs!!!! It works!!!
 
-fileName = 'sens.log';  %D:\sens.log';
-data = textread(fileName,'%s','delimiter',' ');%/n
-temp = find(strcmp(data,'T9'));
-camerawall3 = find(strcmp(data,'3:'));
-%Everytime temp finds one give it a number n+1 for time
-time = [1:5709];
-plot(time,temp)
-%format long
-%disp(temp)
+fileName = 'sens.log';  %set a file name
+data = textread(fileName,'%s','delimiter',' '); %read file
+
+%Plot for CameraWall 3
+temp = find(strcmp(data,'T9')); %check for T9 in data, kind of useless now
+m2 = find(strcmp(data,'3:')); %check for 3: in data
+moto2 = m2 + 1; %finds the temperature
+motor2 = data(moto2); %converts from a line number into the temp value
+motortemp = str2double(motor2); %converts camerawall from a cell into double
+time = [1:6225]; %sets up time variable
+plot(time, motortemp) %plot the graph
+title('Camera Wall 3 Temp');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
+%Plot for Ambient External
+amb = find(strcmp(data,'8:')); %check for 3: in data
+ambient = amb + 1; %finds the temperature
+ambientx = data(ambient); %converts from a line number into the temp value
+ambientexternal = str2double(ambientx); %converts camerawall from a cell into double
+time = [1:6225]; %sets up time variable
+plot(time, ambientexternal) %plot the graph
+title('Ambient External Temp');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
+%Plots temperature for motor driver
+m = find(strcmp(data,'9:')); %check for 3: in data
+moto = m + 1; %finds the temperature
+motor = data(moto); %converts from a line number into the temp value
+motorDriver = str2double(motor); %converts camerawall from a cell into double
+time = [1:6225]; %sets up time variable
+plot(time, motorDriver) %plot the graph
+title('Motor Driver Temp');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
+%Temperature Motor Plot
+m2 = find(strcmp(data,'5:')); %check for 3: in data
+moto2 = m2 + 1; %finds the temperature
+motor2 = data(moto2); %converts from a line number into the temp value
+motortemp = str2double(motor2); %converts camerawall from a cell into double
+time = [1:5709]; %sets up time variable
+plot(time, motortemp) %plot the graph
+title('Motor Temperature');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Weird!
+%Camera wall 2 plot
+a = find(strcmp(data,'2:')); %check for 3: in data
+amb2 = a + 1; %finds the temperature
+ambient = data(amb2); %converts from a line number into the temp value
+ambientinternal = str2double(ambient); %converts camerawall from a cell into double
+time2 = [1:6225]; %sets up time variable
+plot(time2, ambientinternal) %plot the graph
+title('Camera Wall 2 Temp');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
+%Ambient internal plot
+a = find(strcmp(data,'7:')); %check for 3: in data
+amb2 = a + 1; %finds the temperature
+ambient = data(amb2); %converts from a line number into the temp value
+ambientinternal = str2double(ambient); %converts camerawall from a cell into double
+time2 = [1:6225]; %sets up time variable
+plot(time2, ambientinternal) %plot the graph
+title('Ambient Internal Temp');
+xlabel('Time (s)');
+ylabel('Temp (F)');
+
 
